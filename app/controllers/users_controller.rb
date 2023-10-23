fb985c72f468a86483767f874d67f39ec1cadda9
teruyayuu <zhaowuyouzuo@gmail.com>
2023-10-23 15:52:24 +0900

    resources完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.order(id: :asc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def create
+    user = User.create!(user_params)
+    redirect_to user_path(user)
+   end
+
+   def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    redirect_to user_path(user)
+   end
+
+   def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    redirect_to users_path
+   end
+
+   private
+
+   def user_params
+      params.require(:user).permit(:name, :age)
+   end
+end
